9150cb8a2c1c6dabfd6c9da6c86643f20f296bb7
JereUR <jereur.jdv@gmail.com>
2022-09-07 04:28:25 -0300

    Express.js, EJS and templates

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
-const { fruits, money } = require("./fruits");
+/* const http = require("http");
+const server = http.createServer((req, res) => {
+  res.end("I'm responding to your request");
+});
+const port = 3000;
+server.listen(port, () => {
+  console.log("Listening to requests");
+}); */
 
-const cowsay = require("cowsay");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-console.log(
-  cowsay.say({
-    text: "Say whaaaats?",
-    e: "^^",
-    T: "U",
-  })
-);
+//Template engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
-fruits.forEach((item) => {
-  console.count(item);
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "My dynamic title" });
 });
 
-console.log(money);
+app.get("/services", (req, res) => {
+  res.render("services", { servicesTitle: "My dynamic services title" });
+});
+
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .render("404", {
+      errorTitle: "Error 404",
+      errorDescription: "No page found",
+    });
+});
+
+app.listen(port, () => {
+  console.log("Server at your service in the port", port);
+});
