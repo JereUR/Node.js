68a03526df20a73513adbcfcd0c7d905784ce2d7
JereUR <jereur.jdv@gmail.com>
2022-09-09 04:27:53 -0300

    Add pets and link with MongoDB

--- router/Pets.js
+++ router/Pets.js
@@ -14,4 +14,20 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/create", (req, res) => {
+  res.render("create");
+});
+
+router.post("/", async (req, res) => {
+  const body = req.body;
+  try {
+    /* const petDB = new Pet(body);
+    await petDB.save(); */
+    await Pet.create(body);
+    res.redirect("/pets");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;
